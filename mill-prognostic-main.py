5bf531f8cc2c3866959586b7ae6d779fd22a4674
TheRandomFox <abdhalim123@hotmail.com>
2022-02-22 00:25:17 +0800

    Redid feature sorting code
    Each feature is now its own 1D array, separate versions for diff materials.
    It's inefficient as fuck but I wish I knew a better way.

--- mill-prognostic-main.py
+++ mill-prognostic-main.py
@@ -11,7 +11,8 @@ Credit: K.Goebel & A.Agogino
 #import packages
 from sys import exit
 from scipy.io import loadmat
-#import matplotlib
+import numpy as np
+import matplotlib
 
 #read file; extract contents of 'mill' key and unused flat dimension
 #original format == dict, from MATLAB array
@@ -23,26 +24,63 @@ except:
     exit("Error: 'Mill.mat' either does not exist or could not be read.")
 
 #select features to use
-#current:    feats_curr = feed, material, vb, time, smcac, smcdc
-#vibration:  feats_vib = feed, material, vb, time, vib_table, vib_spindle
-#acoustic:   feats_ae = feed, material, vb, time,  ae_table, ae_spindle
+#current:    feats_curr = wearRate, feed, material, vb, time, smcac, smcdc
+#vibration:  feats_vib = wearRate, feed, material, vb, time, vib_table, vib_spindle
+#acoustic:   feats_ae = wearRate, feed, material, vb, time,  ae_table, ae_spindle
 
-#for each experiment run, separate by material and append to relevant feats array
-feats_curr_iron = []
-feats_curr_steel = []
-feats_vib_iron = []
-feats_vib_steel = []
-feats_ae_iron = []
-feats_ae_steel = []
+#extract into individual feats. Separate by material. flatten.
 
-feats = []
-for i in range(3):
+#xxx1 == cast iron; xxx2 == steel
+time1 = []
+feed1 = []
+wearRate1 = []
+smcac1 = []
+smcdc1 = []
+vibtable1 = []
+vibspindle1 = []
+aetable1 = []
+aespindle1 = []
+time2 = []
+feed2 = []
+wearRate2 = []
+smcac2 = []
+smcdc2 = []
+vibtable2 = []
+vibspindle2 = []
+aetable2 = []
+aespindle2 = []
+
+for x in range(len(milldat)):
+    if milldat[x][2][0] > 0 and milldat[x][3][0] > 0:    #if contains valid values for VB and time; else skip.
+        if milldat[x][6][0] == 1:       #material == castiron
+            time1.append(milldat[x][3])
+            feed1.append(milldat[x][5])
+            wearRate1.append(milldat[x][2] / milldat[x][3])      #VB/time (mm/s)
+            smcac1.append(milldat[x][7].flatten())
+            smcdc1.append(milldat[x][8].flatten())
+            vibtable1.append(milldat[x][9].flatten())
+            vibspindle1.append(milldat[x][10].flatten())
+            aetable1.append(milldat[x][11].flatten())
+            aespindle1.append(milldat[x][12].flatten())
+        else:       #material == steel
+            time2.append(milldat[x][3])
+            feed2.append(milldat[x][5])
+            wearRate2.append(milldat[x][2] / milldat[x][3])
+            smcac2.append(milldat[x][7].flatten())
+            smcdc2.append(milldat[x][8].flatten())
+            vibtable2.append(milldat[x][9].flatten())
+            vibspindle2.append(milldat[x][10].flatten())
+            aetable2.append(milldat[x][11].flatten())
+            aespindle2.append(milldat[x][12].flatten())
+
+
+'''
     for x in range(len(milldat)):
-        if milldat[x][2][0] > 0 and milldat[x][3][0] > 0:    #if contains valid values for time and VB; else skip.
-            feats.append(milldat[x][2][0])      #vb
+        if milldat[x][2][0] > 0 and milldat[x][3][0] > 0:    #if contains valid values for VB and time; else skip.
+            wearRate = milldat[x][2][0] / milldat[x][3][0]      #VB/time (mm/s)
+            feats.append(wearRate)
             feats.append(milldat[x][3][0])      #time
             feats.append(milldat[x][5][0])      #feed
-
             if i == 0:      #current feats
                 feats.append(milldat[x][7].flatten())   #smcAC
                 feats.append(milldat[x][8].flatten())   #smcDC
@@ -68,9 +106,11 @@ for i in range(3):
                     feats_ae_steel.append(feats)
 
             feats = []   #reset feats
-
+'''
+#plotx =
+#ploty =
+#plot()
 #input features
-
 
 #output features
 #remaining useful life => not obtainable. insufficient information.
