4fe46f375ebe90b9e128528f7159ba31c454e3e0
TheRandomFox <abdhalim123@hotmail.com>
2022-02-25 14:45:33 +0800

    Update plotdata.py

--- plotdata.py
+++ plotdata.py
@@ -9,15 +9,21 @@ Credit: K.Goebel & A.Agogino
 """
 
 import matplotlib.pyplot as plt
-import matplotlib.axes as ax
-import matplotlib.spines
+import seaborn as sb
 import numpy as np
 
-def plotGraph(ndarray, int):
+def plotGraph(milldat, cutNo):
     '''
     Visualise data for a given cut number
+
+    Parameters:
+        milldat : ndarray
+        cutNo : int
+
+    Returns:
+        void
     '''
-    global milldat, cutNo
+
     datlabels = ['AC motor current',
                  'DC motor current',
                  'Vibration at table',
@@ -26,29 +32,32 @@ def plotGraph(ndarray, int):
                  'AE at spindle']
 
     #generate 6 graphs in a column
-    fig, axs = plt.subplots(6, 1, sharey=True, figsize=[4,6], dpi=130,
-                            frameon=False)
-    axs.set_title('Cut #'+ cutNo)
+    fig, axs = plt.subplots(6, 1, sharey=True, linewidth=0.5, figsize=[4,5], dpi=150)
+    sb.set_theme(context='notebook', font_scale=0.5)
 
-    #define axes for each graph
-    for i in 6:
+    axs[0].set_title('Cut #'+ str(cutNo+1), fontsize='medium')
+
+    #define axses for each graph
+    for i in range(6):
         #x divisions: seconds == no. of readings/Hertz
-        axs[i].plot(np.arange(0,9000)/250, milldat[cutNo][i+7])
+        axs[i].plot(np.arange(0,len(milldat[cutNo][i+7]))/250, milldat[cutNo][i+7])
 
         #set Y label name
-        axs[i].set_ylabel(datlabels[i])
+        axs[i].set_ylabel(datlabels[i], fontsize='small')
 
         #set X label name at the bottom
         if i < 5:
             #if not at bottom, don't show borders
-            axs.spines[:].set_visible(False)
-            axs.set_xticks(labels=None)
-            axs.set_yticks(labels=None)
+            axs[i].spines[:].set_visible(False)
+            axs[i].set_xticks([])
+            axs[i].set_yticks([])
         else:
             #if at bottom, show bottom border
-            axs.spines[['top','left','right']].set_visible(False)
-            axs.spines['bottom'].set_visible(True)
-            axs[i].set_xlabel('Time (s)')
+            axs[i].spines[['top','left','right']].set_visible(False)
+            axs[i].spines['bottom'].set_visible(True)
+            axs[i].set_xlabel('Time (s)', size=5)
+            axs[i].set_yticks([])
+
 
     plt.show()
 
